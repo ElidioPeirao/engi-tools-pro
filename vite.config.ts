99addcb5874845fa6066d9debff150131dca11af
ElidioPeirao <ElidioPeiraoJunior@gmail.com>
2025-05-01 12:10:05 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// substitua 'NOME-DO-REPOSITORIO' pelo nome exato do repositório no GitHub
+const repoName = "NOME-DO-REPOSITORIO";
+
 export default defineConfig(({ mode }) => ({
+  base: `/${repoName}/`,
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
